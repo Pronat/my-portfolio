81e17f28d44aaace6241b576ddf18445587f618e
AlexPron <Fotolinza@gmail.com>
2022-07-15 22:00:37 +0300

    lesson1-5

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -4,7 +4,11 @@ import style from './Nav.module.css'
 export const Nav = () => {
     return (
         <div className={style.nav}>
-
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
         </div>
     );
 };
