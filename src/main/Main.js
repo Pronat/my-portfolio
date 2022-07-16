22391a9d047c86baeb96d58a2d2a7bb93b3917a8
AlexPron <Fotolinza@gmail.com>
2022-07-16 13:46:15 +0300

    lesson1-15

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,6 +4,7 @@ import s from './Main.module.css'
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
+            <div className={s.container}></div>
             <div className={s.text}>
                 <spam>Hi There</spam>
                 <h1>I am Jeffrey Aaron</h1>
