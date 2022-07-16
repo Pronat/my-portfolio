07d0c465e0bddedb1faa5ee6d99f59df27511bb1
AlexPron <Fotolinza@gmail.com>
2022-07-16 15:33:51 +0300

    lesson1-16

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import s from './Main.module.css'
+import sContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.container}></div>
-            <div className={s.text}>
-                <spam>Hi There</spam>
-                <h1>I am Jeffrey Aaron</h1>
-                <p>Frontend Developer</p>
+            <div className={sContainer.container}>
+                <div className={s.text}>
+                    <spam>Hi There</spam>
+                    <h1>I am Jeffrey Aaron</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
             </div>
-            <div className={s.photo}></div>
-
         </div>
     );
 };
