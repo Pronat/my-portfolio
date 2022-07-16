ccda544028b3bb728ef6c02a722eee6833d41926
AlexPron <Fotolinza@gmail.com>
2022-07-16 09:16:59 +0300

    lesson1-13

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,6 +4,8 @@ import s from './Main.module.css'
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
+            <div className={s.text}></div>
+            <div className={s.photo}></div>
 
         </div>
     );
