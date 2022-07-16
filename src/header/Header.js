b95a8c6121e10a08f530356d916c9d939a157fb8
AlexPron <Fotolinza@gmail.com>
2022-07-16 16:45:36 +0300

    lesson1-18

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Header.module.css'
 
 export const Header = () => {
     return (
-        <div>
+        <div className={style.header}>
 
         </div>
     );
