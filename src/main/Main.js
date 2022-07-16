828307081d69c1fdfd88af9488553bd64cc159ef
AlexPron <Fotolinza@gmail.com>
2022-07-16 22:15:18 +0300

    lesson1-24

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,13 +4,14 @@ import style from './Main.module.css'
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={style.text}>
-                <span>Привет</span>
-                <h1>Меня зовут Александр Суворов</h1>
-                <p>Я front-end разработчик</p>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>Привет</span>
+                    <h1>Меня зовут Александр Суворов</h1>
+                    <p>Я front-end разработчик</p>
+                </div>
+                <div className={style.photo}>photo</div>
             </div>
-            <div className={style.photo}>photo</div>
-
         </div>
     );
 };
