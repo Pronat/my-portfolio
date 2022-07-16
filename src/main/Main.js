0d31858175f91893d667a259d1ccbb8d2d568b58
AlexPron <Fotolinza@gmail.com>
2022-07-16 22:34:54 +0300

    lesson1-25

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Main.module.css'
+import styleContainer from '../common/styles/Container.module.css'
 
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={style.container}>
+            <div className={styleContainer.container}>
                 <div className={style.text}>
                     <span>Привет</span>
                     <h1>Меня зовут Александр Суворов</h1>
