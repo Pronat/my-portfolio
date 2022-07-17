0d1dbf56657d67f8368dea00588f0285d59441f5
AlexPron <Fotolinza@gmail.com>
2022-07-17 22:46:43 +0300

    lesson1-32

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -8,9 +8,11 @@ export const Skills = () => {
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
-                <Skill title={"JS"} description={"aaaaaaaaaaa  aaaaa aa aaaaaaa aaaaaa aaaaa aaaaa"}/>
-                <Skill title={"CSS"} description={"bbb bbb bbbbbbb b bbb bbbbbbbbbbbbb bbbbbbbbbbb"}/>
-                <Skill title={"React"} description={"cccccccccc cccccccccc cccccccccccc c c c ccccc"}/>
+                <div className={style.skills}>
+                    <Skill title={"JS"} description={"aaaaaaaaaaa  aaaaa aa aaaaaaa aaaaaa aaaaa aaaaa"}/>
+                    <Skill title={"CSS"} description={"bbb bbb bbbbbbb b bbb bbbbbbbbbbbbb bbbbbbbbbbb"}/>
+                    <Skill title={"React"} description={"cccccccccc cccccccccc cccccccccccc c c c ccccc"}/>
+                </div>
             </div>
 
         </div>
