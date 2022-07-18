adc9c8801a9692c0f40f8b3d47b0d10c91ece5c5
AlexPron <Fotolinza@gmail.com>
2022-07-18 22:36:47 +0300

    lesson1-40

--- src/myworks/Work/Work.js
+++ src/myworks/Work/Work.js
@@ -3,12 +3,10 @@ import style from "./Work.module.css"
 
 export const Work = (props) => {
     return (
-        <div className={style.workBlock}>
+        <div className={style.workModule}>
             <div className={style.image}>{props.image}</div>
-            <div className={style.titDescr}>
             <h3 className={style.title}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
-            </div>
         </div>
     );
 };
