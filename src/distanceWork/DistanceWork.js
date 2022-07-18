6a10f3b4c36c3d4f71b6ac4228867da4554ccb7a
AlexPron <Fotolinza@gmail.com>
2022-07-18 23:39:01 +0300

    lesson1-45

--- src/distanceWork/DistanceWork.js
+++ src/distanceWork/DistanceWork.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import style from './DistanceWork.module.css'
+import styleContainer from '../common/styles/Container.module.css'
+
 export const DistanceWork = () => {
     return (
         <div className={style.DistanceWork}>
-
+            <div className={`${styleContainer.container} ${style.container}`}>
+                    <h3>Рассмотрю варианты удаленной работы</h3>
+                    <button>Нанять меня</button>
+            </div>
         </div>
     );
 };
