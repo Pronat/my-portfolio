6022fca144ce16c6217b87dbdb96382833e34e53
AlexPron <Fotolinza@gmail.com>
2022-07-18 22:23:56 +0300

    lesson1-38

--- src/myworks/MyWorks.js
+++ src/myworks/MyWorks.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import style from './MyWorks.module.css'
 import styleContainer from '../common/styles/Container.module.css'
+import {Work} from "./Work/Work";
 
-export const MyWorks = (props) => {
+export const MyWorks = () => {
     return (
         <div className={style.myWorksBlock}>
             <div className={`${styleContainer.container} ${style.myWorksContainer}`}>
-                <h2 className={style.title}>{title}</h2>
+                <h2 className={style.title}>My works</h2>
                 <div className={style.works}>
-                    {Work}
+                    <Work image={"Image1"} title={"My first work"}
+                          description={"Description of my first work, Description of my first work"}/>
+                    <Work image={"Image2"} title={"My second work"}
+                          description={"Description of my second work, Description of my second work"}/>
                 </div>
-
             </div>
-
         </div>
     );
 };
--- src/myworks/Work/Work.js
+++ src/myworks/Work/Work.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import style from "./Work.module.css"
 
-const Work = () => {
+export const Work = (props) => {
     return (
         <div>
-            
+            <div className={style.image}>{props.image}</div>
+            <h3>{props.title}</h3>
+            <span>{props.description}</span>
         </div>
     );
 };
-
-export default Work;
