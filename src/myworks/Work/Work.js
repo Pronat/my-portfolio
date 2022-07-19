203194b6115cadd017640a120258e63af58841d3
AlexPron <Fotolinza@gmail.com>
2022-07-19 21:17:22 +0300

    lesson1-47

--- src/myworks/Work/Work.js
+++ src/myworks/Work/Work.js
@@ -4,8 +4,9 @@ import style from "./Work.module.css"
 export const Work = (props) => {
     return (
         <div className={style.workModule}>
-            <button className={style.button}>Смотреть</button>
-            <div className={style.image}>{props.image}</div>
+            <div className={style.image}>
+                {props.image}{<button className={style.button}
+            >Смотреть</button>}</div>
 
             <h3 className={style.title}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
