cbe53e52a422f6224d361d1c145f9e8bdf1cbff2
AlexPron <Fotolinza@gmail.com>
2022-07-20 20:34:16 +0300

    lesson1-60

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -9,9 +9,9 @@ export const Contacts = () => {
                 <h3>Contacts</h3>
                 <div>
                     <form className={style.form}>
-                        <div className={style.margin}><input value={"input"}/></div>
-                        <div className={style.margin}><input value={"input"}/></div>
-                        <div className={style.margin}><textarea value={"textarea"}></textarea></div>
+                        <input className={style.margin}/>
+                        <input className={style.margin}/>
+                        <textarea className={style.margin}></textarea>
                     </form>
                 </div>
                 <button className={style.buttonSend}>Send</button>
