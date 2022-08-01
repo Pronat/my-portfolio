70bce42d7ec6685512913016d0ce692debcd6834
AlexPron <Fotolinza@gmail.com>
2022-08-01 23:02:49 +0300

    lesson1-66

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -7,13 +7,11 @@ export const Contacts = () => {
         <div className={style.contacts}>
             <div className={`${styleContainer.container} ${style.container}`}>
                 <h3>Contacts</h3>
-                <div>
                     <form className={style.form}>
                         <input className={style.margin}/>
                         <input className={style.margin}/>
                         <textarea className={style.margin}></textarea>
                     </form>
-                </div>
                 <button className={style.buttonSend}>Send</button>
             </div>
         </div>
