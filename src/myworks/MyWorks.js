c51582b90bcd5b298c66fca3b1c01f5ed67f48a9
AlexPron <Fotolinza@gmail.com>
2022-12-05 23:38:02 +0300

    lesson2-6

--- src/myworks/MyWorks.js
+++ src/myworks/MyWorks.js
@@ -2,14 +2,13 @@ import React from 'react';
 import style from './MyWorks.module.css'
 import styleContainer from '../common/styles/Container.module.css'
 import {Work} from "./Work/Work";
+import {Title} from "../common/components/title/Title";
 
 export const MyWorks = () => {
     return (
         <div className={style.myWorksBlock}>
             <div className={`${styleContainer.container} ${style.myWorksContainer}`}>
-                <div className={style.title}>
-                    <h2>Projects</h2>
-                </div>
+                <Title />
                 <div className={style.works}>
                     <Work image={"Image1"} title={"My first work"}
                           description={"Description of my first work, Description of my first work"}/>
