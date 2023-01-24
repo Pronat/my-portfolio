b0880cea0ef6d83d286e13372b3e7ec500812d8a
AlexPron <Fotolinza@gmail.com>
2023-01-24 21:10:28 +0300

    lesson2-16

--- src/myworks/Work/Work.js
+++ src/myworks/Work/Work.js
@@ -5,7 +5,7 @@ export const Work = (props) => {
     return (
         <div className={style.workModule}>
             <div className={style.image} style={props.style}>
-                {props.image}{<a href={"ссылка"} className={style.button}>Смотреть</a>}
+                {props.image}{<a href={"ссылка"} className={style.button}>Project</a>}
             </div>
             {/*<div className={style.projectInfo}>*/}
                 <h3 className={style.workTitle}>{props.title}</h3>
