f74dd477b0af3b6f790deaae800b87b2159a59bb
AlexPron <Fotolinza@gmail.com>
2023-01-28 22:33:07 +0300

    lesson2-33

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import style from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
+import avatarImage from './../assets/image/avatar.jpg'
+
 
 export const Main = () => {
+    const avatar = {
+        color: 'blue',
+        backgroundImage: `url(${avatarImage})`,
+    };
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
-                    <span>Привет</span>
-                    <h1>Меня зовут Александр Суворов</h1>
-                    <p>Я front-end разработчик</p>
+                    <span>Hi</span>
+                    <h1>My name is Alexandr Suvorov</h1>
+                    <p>I am front-end developer</p>
                 </div>
-                <div className={style.photo}>photo</div>
+                <div className={style.photo} style={avatar}>Photo</div>
             </div>
         </div>
     );
