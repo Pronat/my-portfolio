4a685d77dfd5f4e27a08380c68fceca446daa431
AlexPron <Fotolinza@gmail.com>
2023-01-28 23:30:54 +0300

    lesson2-35

--- src/skills/Skill/Skill.js
+++ src/skills/Skill/Skill.js
@@ -2,12 +2,9 @@ import React from 'react';
 import style from './Skill.module.scss'
 
 export const Skill = (props) => {
-
-
-
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon} style={props.style}></div>
             <h3 className={style.skillTitle}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
         </div>
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -6,6 +6,9 @@ import {Title} from "../common/components/title/Title";
 import tsImage from "../assets/image/icons/ts.png";
 import jsImage from "../assets/image/icons/js.png";
 import reactImage from "../assets/image/icons/react.jpg";
+import reduxImage from "../assets/image/icons/redux.png";
+import htmlImage from "../assets/image/icons/html.png";
+import cssImage from "../assets/image/icons/css.png";
 
 export const Skills = () => {
     const tsIcon = {
@@ -16,21 +19,34 @@ export const Skills = () => {
         color: 'blue',
         backgroundImage: `url(${jsImage})`,
     };
+    const reduxIcon = {
+        color: 'blue',
+        backgroundImage: `url(${reduxImage})`,
+    };
     const reactIcon = {
         color: 'blue',
         backgroundImage: `url(${reactImage})`,
     };
+    const cssIcon = {
+        color: 'blue',
+        backgroundImage: `url(${cssImage})`,
+    };
+    const htmlIcon = {
+        color: 'blue',
+        backgroundImage: `url(${htmlImage})`,
+    };
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={"Skills"}/>
                 <div className={style.skills}>
-                    <Skill title={"JS"} description={"язык программирования, который позволяет вам создать динамически обновляемый контент"}/>
-                    <Skill title={"TS"} description={"язык программирования для веб-разработки, основанный на JavaScript"}/>
-                    <Skill title={"HTML"} description={"это код, который используется для структурирования и отображения веб-страницы и её контента"}/>
-                    <Skill title={"CSS"} description={"Cascading Style Sheets — это каскадные таблицы стилей"}/>
-                    <Skill title={"React"} description={"библиотеку для создания пользовательских интерфейсов"}/>
-                    <Skill title={"Redux"} description={"контейнер предсказуемых состояний для JavaScript приложений"}/>
+                    <Skill style={jsIcon} title={"JS"} description={"язык программирования, который позволяет вам создать динамически обновляемый контент"}/>
+                    <Skill style={tsIcon} title={"TS"} description={"язык программирования для веб-разработки, основанный на JavaScript"}/>
+                    <Skill style={htmlIcon} title={"HTML"} description={"это код, который используется для структурирования и отображения веб-страницы и её контента"}/>
+                    <Skill style={cssIcon} title={"CSS"} description={"Cascading Style Sheets — это каскадные таблицы стилей"}/>
+                    <Skill style={reactIcon} title={"React"} description={"библиотеку для создания пользовательских интерфейсов"}/>
+                    <Skill style={reduxIcon} title={"Redux"} description={"контейнер предсказуемых состояний для JavaScript приложений"}/>
                 </div>
             </div>
 
