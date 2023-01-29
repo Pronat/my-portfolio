c727eda8612fb74f172cd52fcd01d9883e675e4f
AlexPron <Fotolinza@gmail.com>
2023-01-29 17:48:44 +0300

    lesson2-46

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import style from './Footer.module.css'
+import style from './Footer.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 
 export const Footer = () => {
     return (
         <div className={style.footer}>
             <div className={`${styleContainer.container} ${style.container}`}>
-                <h3>Суворов Александр</h3>
+                <h3 className={style.footerTitle}>Суворов Александр</h3>
                 <div className={style.icons}>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                 </div>
-                <h3>2019 Все права защищены</h3>
+                <h3 className={style.footerDescription}>2019 Все права защищены</h3>
             </div>
 
         </div>
