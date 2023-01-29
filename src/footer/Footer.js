52163cb2c1ce125563256b69e3658c4ccf90f657
AlexPron <Fotolinza@gmail.com>
2023-01-29 19:42:21 +0300

    lesson2-47

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -6,14 +6,14 @@ export const Footer = () => {
     return (
         <div className={style.footer}>
             <div className={`${styleContainer.container} ${style.container}`}>
-                <h3 className={style.footerTitle}>Суворов Александр</h3>
+                <h3 className={style.footerTitle}>Alexandr Suvorov</h3>
                 <div className={style.icons}>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                     <div className={style.icon1}></div>
                 </div>
-                <h3 className={style.footerDescription}>2019 Все права защищены</h3>
+                <h3 className={style.footerDescription}>2019 All rights reserved</h3>
             </div>
 
         </div>
